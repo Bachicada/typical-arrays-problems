66a1673023b998d0715ff2a841dd2724f8ff40da
Bachicada <bachicada@gmail.com>
2021-08-13 23:33:44 +0300

    docs: done typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if ((array===undefined)||(array.length===0)){ return 0;}
+    const compare = (a, b)=> {
+    return a-b;
+    }
+    array.sort(compare);
+    return array[0]
+  }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if ((array===undefined)||(array.length===0)){ return 0;}
+    const compare = (a, b)=> {
+    return b-a;
+    }
+    array.sort(compare);
+    return array[0]
+  }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ((array===undefined)||(array.length===0)){ return 0;}
+    let sum=0;
+    for (let i=0;i<array.length;i++) {
+                    sum +=array[i];
+                        }
+     return sum/array.length
+     }     
